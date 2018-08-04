d131ad7a5dc925299bdba9b266b5fb508a629c06
bdurb <brian@rpmgreenville.com>
2018-08-04 08:16:08 -0700

    set up Users class and built test to confirm functionality

---
+++ server/utils/users.test.js
@@ -0,0 +1,13 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+  it('should add new user', () => {
+    const users = new Users();
+    const user = {id: 1, name: 'joe', room: 'joes room'}
+    const resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+});
