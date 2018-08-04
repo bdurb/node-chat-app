b0f598cbdb8b1a13ae9d108ad18bdc70ecfde9c6
bdurb <brian@rpmgreenville.com>
2018-08-03 20:24:34 -0700

    added time stamps via moment

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,9 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message', message);
+  const formattedTime = moment(message.createdAt).format('h:mm a')
   const li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`)
+  li.text(`${message.from} ${formattedTime}: ${message.text}`)
 
   jQuery('#messages').append(li);
 });
@@ -31,10 +31,11 @@ jQuery('#message-form').on('submit', function (e) {
 });
 
 socket.on('newLocationMessage', function(message) {
+  const formattedTime = moment(message.createdAt).format('h:mm a')
   const li = jQuery('<li></li>');
   const a = jQuery('<a target="_blank">My Current Location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
