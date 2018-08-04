c1f9fb0c4137f5614219ae08731ec5c67e126dc4
bdurb <brian@rpmgreenville.com>
2018-08-04 07:45:00 -0700

    added validation for name and room name for chat app, created new test to verify outcome

---
+++ server/utils/validation.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect')
+
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non string value', () => {
+    const result = isRealString(98);
+    expect(result).toBe(false);
+  });
+  it('should reject string with only spaces', () => {
+    const result = isRealString('     ');
+    expect(result).toBe(false);
+  });
+  it('should allow strings with non-space characters', () => {
+    const result = isRealString('  hello     ');
+    expect(result).toBe(true);
+  });
+});
