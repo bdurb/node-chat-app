aab4a98175f83c0db63476b034c2f4ca00cab8d1
bdurb <brian@rpmgreenville.com>
2018-08-03 20:45:35 -0700

    added message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,10 +10,14 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   const formattedTime = moment(message.createdAt).format('h:mm a')
-  const li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
+  const template = jQuery('#message-template').html();
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 
@@ -32,13 +36,14 @@ jQuery('#message-form').on('submit', function (e) {
 
 socket.on('newLocationMessage', function(message) {
   const formattedTime = moment(message.createdAt).format('h:mm a')
-  const li = jQuery('<li></li>');
-  const a = jQuery('<a target="_blank">My Current Location</a>');
+  const template = jQuery('#location-message-template').html();
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 const locationbutton = jQuery('#send-location');
